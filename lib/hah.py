2ce836d5ce4e335e62f43ba4fcdb23487e7bf4c5
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-04-15 18:18:26 +0100

    added QUICKSTART, hah & lib/hah.py & test/test_hah.py

---
+++ lib/hah.py
@@ -0,0 +1,7 @@
+
+class Hah():
+	def __init__(self, nick):
+		self.nick = nick
+	def connect(self, host):
+		print "connecting to %s.. NOT!" % host
+
---
+++ tests/test_hah.py
@@ -0,0 +1,11 @@
+import unittest
+import hah
+
+class TestHah(unittest.TestCase):
+	def testWhat(self):
+		# 0)> do something.
+		# 1)> assert someting.
+		pass
+
+if __name__ == "__main__":
+    unittest.main()
