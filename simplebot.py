27edd0e2bfde4204d67eec0f7ea431d17c361a43
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-04-15 16:30:58 +0100

    added simplebot.py from http://oreilly.com/pub/h/1968

---
+++ simplebot.py
@@ -0,0 +1,30 @@
+# http://oreilly.com/pub/h/1968
+# IRC HACKS #36> A Simple Python IRC Client
+
+import sys
+import socket
+import string
+
+HOST="irc.freenode.net"
+PORT=6667
+NICK="MauBot"
+IDENT="maubot"
+REALNAME="MauritsBot"
+readbuffer=""
+
+s=socket.socket( )
+s.connect((HOST, PORT))
+s.send("NICK %s\r\n" % NICK)
+s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+
+while 1:
+    readbuffer=readbuffer+s.recv(1024)
+    temp=string.split(readbuffer, "\n")
+    readbuffer=temp.pop( )
+
+    for line in temp:
+        line=string.rstrip(line)
+        line=string.split(line)
+
+        if(line[0]=="PING"):
+            s.send("PONG %s\r\n" % line[1])
