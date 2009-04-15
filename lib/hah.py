efd7c3dbc38ffe2b890ae05b499a6d8b43eec5a7
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-04-15 19:32:33 +0100

    debug> enabled.

--- lib/hah.py
+++ lib/hah.py
@@ -1,7 +1,23 @@
+import ircbot
 
-class Hah():
-	def __init__(self, nick):
-		self.nick = nick
-	def connect(self, host):
-		print "connecting to %s.. NOT!" % host
+class Hah(ircbot.SingleServerIRCBot):
+	def __init__(self, nick, channel, server):
+		self.channel = channel
+		servers = [(server, 6667)]
+		ircbot.SingleServerIRCBot.__init__(self, servers, nick, 'hah!')
 
+	def unleash(self):
+		print 'unleashed.'
+		self.start()
+		self.connect()
+
+	## triggers.
+	def on_welcome(self, c, e):
+		print 'joining %s' % self.channel
+		c.join(self.channel)
+
+	def on_privmsg(self, c, e):
+		print '> %s: %s' % (c, str(e.arguments()))
+
+	def on_pubmsg(self, c, e):
+		print '%s: %s' % (c, str(e.arguments()))
