f435ee03bd1a9612c26f920cb661d82ffa37f7eb
Fredrik Mollerstrand <fredrik@mollerstrand.se>
2009-04-19 12:22:16 +0100

    thread ctcp action updates.

--- lib/hah.py
+++ lib/hah.py
@@ -40,14 +40,13 @@ class Hah(ircbot.SingleServerIRCBot):
 			nick = irclib.nm_to_n(e.source())
 			msg = e.arguments()[1]
 			update = nick + " " + msg
-			self.twitter_post(update)
+			thread.start_new.thread(self.twitter_post, update)
 
 	def on_pubmsg(self, c, e):
 		channel = e.target()
 		nick = irclib.nm_to_n(e.source())
 		msg = e.arguments()[0]
 		print "<%s%s> %s" % (nick, channel, msg)
-
 		thread.start_new_thread(self.twitterism, (c, msg, nick, channel))
 
 	# parse message, react to messages matching regex.
